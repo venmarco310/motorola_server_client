6cf1be90230419a174fcd1a1c060fa030b71e82e
Marco Venegas <veneng310@gmail.com>
2026-09-13 13:34:02 -0700

    Created a script that can generate as many lines as a dev wants. Can be used with manual_test.py

---
+++ generate_numbers_txt.py
@@ -0,0 +1,8 @@
+# Open a file named 'numbers.txt' in write mode ('w')
+with open("./cornell_movie_quotes_corpus/numbers.txt", "w") as file:
+    # Loop from 1 to 1,000,000
+    for number in range(1, 1000001):
+        # Write the number followed by a newline character
+        file.write(f"i = {number}\n")
+
+print("File 'numbers.txt' has been created successfully.")
